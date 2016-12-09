ee534c01b3e7cea4ba1e8c8918bf1040e5863bfb
Ngbt95 <Mr.LinK.lin@gmail.com>
2016-12-09 23:52:41 +0500

    gallery

--- gallery.js
+++ gallery.js
@@ -1,74 +1,87 @@
-var mas = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg']
+function load(){
+	path = 'ngbtl95.github.io/gallery.html'
+	url = window.location.toString();
+	if (url[url.length - 1] == '0'){
+		localStorage['opened_img'] = 0
+		window.location.assign(path)
+	}
+	opened_img = localStorage['opened_img'];
+ 	if (localStorage['opened_img'] != 0)
+ 		open_img(opened_img)
+ 	
+ }
 
-
-function open_img(obj){
-event.preventDefault();
- var id = obj.id
-
- var img = document.getElementById(id);
- var addr = img.src;
-
-
-
- img_name = addr.match(/([0-9]+)(\.jpg)/i)//[1] - 1;
- img_number = Number(img_name[1]);
- img_name = img_name[0]
-
-
- addr = addr.replace("little-image", "big-image");
- var div = document.createElement('div');
- div.className = "popup";
- container = document.getElementsByClassName('container')[0]
- popup_height = container.offsetHeight + 'px'
- div.style.height = popup_height;
- document.body.appendChild(div)
- var div2 = document.createElement('div');
- div2.className = "popup_bg";
- div.appendChild(div2);
- var a = document.createElement('a');
- a.href = "";
- a.id = "modal_close";
- a.innerHTML = "X";
- div2.appendChild(a);
-
- var loading = document.createElement('p')
- img.id = 'loading'
- loading.innerHTML = 'Loading...'
- loading.style.color = '#fff'
- loading.style.fontSize = '40px'
- loading.style.height = '20px'
- loading.style.marginTop = '25%'
- div2.appendChild(loading);
-
- 
- var img = document.createElement('img');
- img.src = addr;
- img.className = "popup_img"
- div.appendChild(img);
-
-
- addEventListener('keydown', function close(event){
- 	event.preventDefault();
- 	if (event.keyCode == 27){
- 		var el = document.getElementsByClassName('popup')[0];
+function close_img(){
+	var el = document.getElementsByClassName('popup')[0];
  		el.parentNode.removeChild(el);
-		removeEventListener('keydown', close)
+		localStorage['opened_img'] = 0;
+}
+
+function create_popup(addr){
+	var path = "ngbtl95.github.io/gallery.html?"
+
+	var popup = document.createElement('div');
+ 	popup.className = "popup";
+ 	container = document.getElementsByClassName('container')[0]
+ 	popup_height = container.offsetHeight + 'px'
+ 	popup.style.height = popup_height;
+ 	document.body.appendChild(popup)
+
+ 	var popup_bg = document.createElement('div');
+ 	popup_bg.className = "popup_bg";
+ 	popup.appendChild(popup_bg);
+
+ 	var modal_close = document.createElement('a');
+  	modal_close.id = "modal_close";
+	modal_close.innerHTML = "X";
+ 	modal_close.href = path + '0';
+ 	popup_bg.appendChild(modal_close);
+
+ 	var loading = document.createElement('p')
+ 	loading.class = 'loading'
+ 	loading.innerHTML = 'Loading...'
+ 	popup_bg.appendChild(loading);
+
+ 	var img = document.createElement('img');
+ 	img.src = addr;
+ 	img.className = "popup_img"
+ 	popup.appendChild(img);
+
+ 	return(img);
+}
+
+function open_img(id){
+    var mas = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg', '6.jpg', '7.jpg',
+ 	'8.jpg', '9.jpg']
+ 	var img = document.getElementById(id);
+ 	var addr = img.src;
+ 	var img_name = addr.match(/([0-9]+)(\.jpg)/i);
+ 	var img_number = Number(img_name[1]);
+ 	img_name = img_name[0]
+ 	addr = addr.replace("little-image", "big-image");
+	localStorage.setItem('opened_img', id);
+	img = create_popup(addr)
+ 	addEventListener('keydown', function keydownListen(event){
+ 		if (event.keyCode == 27){
+ 			close_img();
+ 			removeEventListener('keydown', keydownListen) 		
  		}
- 	if (event.keyCode == 39){ //right
- 		if (img_number < mas.length)
- 			img_number++
- 		else
- 			img_number = 1
- 		img.src = addr.replace(img_name, img_number + '.jpg')
+ 		if (event.keyCode == 39){ //right
+ 			if (img_number < mas.length)
+ 				img_number++;
+ 			else
+ 				img_number = 1;
+ 			img.src = addr.replace(img_name, img_number + '.jpg');
+ 			localStorage['opened_img'] = img_number;
  		}
- 	if (event.keyCode == 37){ //left
- 		if (img_number > 1)
- 			img_number--
- 		else
- 			img_number = mas.length
- 		img.src = addr.replace(img_name, img_number + '.jpg')
+ 		if (event.keyCode == 37){ //left
+ 			if (img_number > 1)
+ 				img_number--;
+ 			else
+ 				img_number = mas.length;
+ 			img.src = addr.replace(img_name, img_number + '.jpg');
+ 			localStorage['opened_img'] = img_number;
  		}
  });
-
 return;
 }
